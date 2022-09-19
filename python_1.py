d388ed4b8d8241b598e10aa6863bc6b0289035c1
harshad-leo <harshadsalunkhe99@gmail.com>
2022-09-19 18:49:56 +0100

    My second comment.

--- python_1.py
+++ python_1.py
@@ -1,10 +1,3 @@
-numbers = [12, 75, 150, 180, 145, 525, 50]
-# iterate each item of a list
-for item in numbers:
-    if item > 500:
-        break
-    elif item > 150:
-        continue
-    # check if number is divisible by 5
-    elif item % 5 == 0:
+    # check if number is divisible by 2
+    elif item % 2 == 0:
         print(item)
